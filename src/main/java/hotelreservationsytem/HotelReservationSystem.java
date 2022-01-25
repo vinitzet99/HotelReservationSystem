aa82e8e9128f4e048a80f6176ccb68b106547e39
vinit <vinitsxn@gmail.com>
2022-01-25 08:21:45 +0530

    Added: Welcome Message added

---
+++ src/main/java/hotelreservationsytem/HotelReservationSystem.java
@@ -0,0 +1,12 @@
+/**
+ * @uthor: Vinit Kumar
+ * @created on: 25-Jan-2021
+ * Hotel Reservation System
+ */
+package hotelreservationsytem;
+
+public class HotelReservationSystem {
+    public static void main(String[] args){
+        System.out.println("Welcome to Hotel Reservation System!!!");
+    }
+}
